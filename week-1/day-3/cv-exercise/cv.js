576f068e89cb57e965311f74f7af4b9ae1fdb7f5
Dovid Levine <dovidlevine@Dovids-MacBook-Pro.local>
2024-06-04 22:35:11 +0300

    added mobile menu

--- week-1/day-3/cv-exercise/cv.js
+++ week-1/day-3/cv-exercise/cv.js
@@ -1,3 +1,14 @@
+function openMenu(){
+    document.getElementById('mobile-menu-button').style.display = 'none';
+    document.getElementById('mobile-menu-container').style.display = 'flex';
+}
+
+function closeMenu(){
+    document.getElementById('mobile-menu-button').style.display = 'flex';
+    document.getElementById('mobile-menu-container').style.display = 'none';
+}
+
+
 function sendEmail(event){
     event.preventDefault()
     const subject = document.getElementById('subject-input').value;
