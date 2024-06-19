e257e66fa67a71d1113e8d78ac3999698ae2fe7f
Dovid Levine <dovidlevine@Dovids-MacBook-Pro.local>
2024-06-19 13:10:14 +0300

    uncomment exercise 1

--- week-3/day-4/exercises_xp.py
+++ week-3/day-4/exercises_xp.py
@@ -1,36 +1,36 @@
-# #exercise 1
-# import random
-# def get_words_from_file():
-#     with open("word_list.txt", mode='r') as file:
-#         return file.readlines()
+#exercise 1
+import random
+def get_words_from_file():
+    with open("word_list.txt", mode='r') as file:
+        return file.readlines()
     
-# def get_random_sentence(length: int):
-#     words = get_words_from_file()
-#     sentence = ""
-#     for i in range(length):
-#         sentence += words[random.randint(0, len(words) - 1)].strip().lower() + " "
-#     sentence = sentence.strip()
-#     sentence += '.'
-#     sentence = sentence[0].upper() + sentence[1:]
-#     print(sentence)
-#     return sentence
+def get_random_sentence(length: int):
+    words = get_words_from_file()
+    sentence = ""
+    for i in range(length):
+        sentence += words[random.randint(0, len(words) - 1)].strip().lower() + " "
+    sentence = sentence.strip()
+    sentence += '.'
+    sentence = sentence[0].upper() + sentence[1:]
+    print(sentence)
+    return sentence
 
-# def main():
-#     print("This program will create a random sentence with the number of words that you choose.")
-#     sentence_length = input("Enter the number of words you want the sentence to have (must be between 2 and 20):")
-#     try:
-#         if 1 < int(sentence_length) < 21:
-#             return get_random_sentence(int(sentence_length))
-#         else: 
-#             print("Sentence length must be between 2 and 20")
-#             main()
+def main():
+    print("This program will create a random sentence with the number of words that you choose.")
+    sentence_length = input("Enter the number of words you want the sentence to have (must be between 2 and 20):")
+    try:
+        if 1 < int(sentence_length) < 21:
+            return get_random_sentence(int(sentence_length))
+        else: 
+            print("Sentence length must be between 2 and 20")
+            main()
 
-#     except Exception as e:
-#         print(e) 
-#         print("You must enter a valid number between 2 and 20")
-#         main()
+    except Exception as e:
+        print(e) 
+        print("You must enter a valid number between 2 and 20")
+        main()
 
-# main()
+main()
 
 #exercise 2
 import json
